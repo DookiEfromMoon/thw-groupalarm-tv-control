c57f981386059c50e0ff4fd4c93abf01b42c8928
Michael Reichelt <michael.reichelt@meinemarkenmode.de>
2024-01-31 17:47:42 +0100

    main
    - removed monitorId because is not necessary
    - fixed language

--- config.py
+++ config.py
@@ -2,14 +2,13 @@ import json
 
 
 class Config:
-    def __init__(self, rest_url, api_token, monitor_id, alarm_path):
+    def __init__(self, rest_url, api_token, alarm_path):
         self.restUrl = rest_url
         self.apiToken = api_token
-        self.monitorId = monitor_id
         self.alarmPath = alarm_path
 
 
 def load_config():
     with open("config.json", "r") as file:
         data = json.load(file)
-    return Config(data["restUrl"], data["apiToken"], data["monitorId"], data["alarmPath"])
+    return Config(data["restUrl"], data["apiToken"], data["alarmPath"])
